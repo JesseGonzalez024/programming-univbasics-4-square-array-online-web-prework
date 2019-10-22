0a4417fab4e7f0209bf992abc188e4db6e295020
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-22 02:02:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,19 +9,9 @@ def square_array(array)
     
   
     
-    
-    
-    new_numbers = [num1, num2, num3]
-
-end
+  
     
     
     
     
   
-#def
-  #
- # new_numbers = [num1, num2, num3]
-#end
-
-#new_numbers.to_a
