dc6f56f67b50094e63435b271d58a6a379859598
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-22 02:18:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,13 @@
 def square_array(array)
   counter = 0
+  new_numbers = []
   while array[counter] do
-   = array[counter]**2
+    newarray[counter]**2 
     counter += 1
-    new_numbers = []
+    
+    
+  end
+  
     
     
     
