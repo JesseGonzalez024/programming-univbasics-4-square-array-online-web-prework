efbfbd4dadf39f08e714649a3936c2a5319975c6
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-10-22 01:32:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,11 @@
 def square_array(array)
   while array do
-    arrat
+    
     
     
   
 #def
-  #array[0]**2 = num1
-  #array[1]**2 = num2
-  #array[2]**2 = num3
+  #
  # new_numbers = [num1, num2, num3]
 #end
 
